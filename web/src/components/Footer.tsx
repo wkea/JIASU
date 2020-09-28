e645232863d13c11acdbd232ded8b50d3d2bc4df
wkea <52775998+wkea@users.noreply.github.com>
2020-09-29 02:12:48 +0800

    Update Footer.tsx

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <footer className="footer">
-    <span>built by Brendonovich, mat1jaczyyy, vaaski ©</span>
+    <span>(由LPDBBS社区提加速服务，mat1jaczyyy授权代理)built by Brendonovich, mat1jaczyyy, vaaski ©</span>
     <a
       href="https://github.com/mat1jaczyyy/LP-Firmware-Utility"
       target="_blank"
